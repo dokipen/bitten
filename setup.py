038c76bbcad61f61df6040ee6508bcb80cd9cfe9
cmlenz <cmlenz@2d6df8db-84f7-0310-ba0f-a4c632ebc3b5>
2005-06-19 14:21:20 +0000

    Remove reference to package removed in [25].
    git-svn-id: http://svn.edgewall.org/repos/bitten/trunk@27 2d6df8db-84f7-0310-ba0f-a4c632ebc3b5

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ from bitten import __version__ as VERSION
 from bitten.util.testrunner import unittest
 
 setup(name='bitten', version=VERSION,
-      packages=['bitten', 'bitten.build', 'bitten.setuptools', 'bitten.util'],
+      packages=['bitten', 'bitten.build', 'bitten.util'],
       author="Christopher Lenz", author_email="cmlenz@gmx.de",
       url="http://bitten.cmlenz.net/", cmdclass={'unittest': unittest})
