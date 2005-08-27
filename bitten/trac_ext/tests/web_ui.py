dc97b6e9ca83b873b4c000073b4310f20ae409b2
cmlenz <cmlenz@2d6df8db-84f7-0310-ba0f-a4c632ebc3b5>
2005-08-27 07:58:12 +0000

    Flip the switch: Bitten is now BSD-licensed.
    git-svn-id: http://svn.edgewall.org/repos/bitten/trunk@165 2d6df8db-84f7-0310-ba0f-a4c632ebc3b5

--- bitten/trac_ext/tests/web_ui.py
+++ bitten/trac_ext/tests/web_ui.py
@@ -1,3 +1,12 @@
+# -*- coding: iso8859-1 -*-
+#
+# Copyright (C) 2005 Christopher Lenz <cmlenz@gmx.de>
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://bitten.cmlenz.net/wiki/License.
+
 import unittest
 
 from trac.perm import PermissionCache, PermissionSystem
