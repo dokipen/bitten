f972509f32d3bf9f0a1ea2291c1ae53e8d48fdc0
dfraser <dfraser@2d6df8db-84f7-0310-ba0f-a4c632ebc3b5>
2008-07-16 08:08:24 +0000

    Reverted incorrect parts of r557 - the `os.path.join` is only needed when the filename passed through is generated by `_create_file`
    git-svn-id: http://svn.edgewall.org/repos/bitten/trunk@559 2d6df8db-84f7-0310-ba0f-a4c632ebc3b5

--- bitten/build/tests/pythontools.py
+++ bitten/build/tests/pythontools.py
@@ -75,7 +75,7 @@ test.module     60      60   100%% %s/test/module.py
         child = xml.children[0]
         self.assertEqual('coverage', child.name)
         self.assertEqual('test.module', child.attr['name'])
-        self.assertEqual(os.path.join('test', 'module.py'), child.attr['file'])
+        self.assertEqual('test/module.py', child.attr['file'])
         self.assertEqual(100, child.attr['percentage'])
         self.assertEqual(60, child.attr['lines'])
 
@@ -96,7 +96,7 @@ test.module     60      60   100% ./test/module.py
         child = xml.children[0]
         self.assertEqual('coverage', child.name)
         self.assertEqual('test.module', child.attr['name'])
-        self.assertEqual(os.path.join('test', 'module.py'), child.attr['file'])
+        self.assertEqual('test/module.py', child.attr['file'])
         self.assertEqual(100, child.attr['percentage'])
         self.assertEqual(60, child.attr['lines'])
 
@@ -117,7 +117,7 @@ test.module     28     26    92%   13-14 ./test/module.py
         child = xml.children[0]
         self.assertEqual('coverage', child.name)
         self.assertEqual('test.module', child.attr['name'])
-        self.assertEqual(os.path.join('test', 'module.py'), child.attr['file'])
+        self.assertEqual('test/module.py', child.attr['file'])
         self.assertEqual(92, child.attr['percentage'])
         self.assertEqual(28, child.attr['lines'])
 
@@ -179,7 +179,7 @@ lines   cov%%   module   (path)
         child = xml.children[0]
         self.assertEqual('coverage', child.name)
         self.assertEqual('test.module', child.attr['name'])
-        self.assertEqual(os.path.join('test', 'module.py'), child.attr['file'])
+        self.assertEqual('test/module.py', child.attr['file'])
 
     def test_summary_with_relative_path(self):
         self.summary.write("""
@@ -197,7 +197,7 @@ lines   cov%   module   (path)
         child = xml.children[0]
         self.assertEqual('coverage', child.name)
         self.assertEqual('test.module', child.attr['name'])
-        self.assertEqual(os.path.join('test', 'module.py'), child.attr['file'])
+        self.assertEqual('test/module.py', child.attr['file'])
 
 
 class FigleafTestCase(unittest.TestCase):
@@ -325,7 +325,7 @@ class FilenameNormalizationTestCase(unittest.TestCase):
         filenames = pythontools._normalize_filenames(
                             self.ctxt, [filename],
                             FileSet(self.ctxt.basedir, '**/*.py', None))
-        self.assertEqual([os.path.join('test', 'module.py')], list(filenames))
+        self.assertEqual(['test/module.py'], list(filenames))
 
 
 class UnittestTestCase(unittest.TestCase):
