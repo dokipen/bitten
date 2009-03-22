a9fbb32629511ebc21e0f9f6f2e5fff6095b6190
mgood <mgood@2d6df8db-84f7-0310-ba0f-a4c632ebc3b5>
2009-03-22 23:47:36 +0000

    Fix line endings and trailing whitespace in new notification files
    git-svn-id: http://svn.edgewall.org/repos/bitten/trunk@607 2d6df8db-84f7-0310-ba0f-a4c632ebc3b5

--- bitten/notify.py
+++ bitten/notify.py
@@ -4,7 +4,7 @@
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution. 
+# you should have received as part of this distribution.
 
 from trac.core import *
 from trac.web.chrome import ITemplateProvider
@@ -32,13 +32,13 @@ class BittenNotify(Component):
 
 class BittenNotifyDispatcher(Component):
     """Sends notifications on build status by mail."""
-    
+
     implements(IBuildListener, ITemplateProvider)
-    
+
     def __init__(self):
         self.log.debug('Initializing BittenNotify Dispatcher')
         self.email = BittenNotifyEmail(self.env)
-        
+
     def notify(self, build = None):
         self.log.info('BittenNotify invoked for build %r' % build)
         self.log.debug('build status: %s' % build.status)
@@ -46,19 +46,19 @@ class BittenNotifyDispatcher(Component):
             self.log.info('Sending notification for build %r' % build)
             build_info = BuildInfo(self.env, build)
             self.email.notify(build_info)
-        
+
     def _should_notify(self, build):
-        notify_on_failure = self.config.getbool(CONFIG_SECTION, 
+        notify_on_failure = self.config.getbool(CONFIG_SECTION,
                 NOTIFY_ON_FAILURE)
-        notify_on_success = self.config.getbool(CONFIG_SECTION, 
+        notify_on_success = self.config.getbool(CONFIG_SECTION,
                 NOTIFY_ON_SUCCESS)
         build_is_failure = (build.status == Build.FAILURE)
         build_is_success = (build.status == Build.SUCCESS)
         return (build_is_failure and notify_on_failure) or \
                 (build_is_success and notify_on_success)
-    
+
     # IBuildListener methods
-    
+
     def build_started(self, build):
         """build started"""
         self.notify(build)
@@ -70,15 +70,15 @@ class BittenNotifyDispatcher(Component):
     def build_completed(self, build):
         """build completed"""
         self.notify(build)
-        
+
     # ITemplateProvider methods
-    
+
     def get_templates_dirs(self):
         """Return a list of directories containing the provided template
         files."""
         from pkg_resources import resource_filename
         return [resource_filename(__name__, 'templates')]
-        
+
     def get_htdocs_dirs(self):
         """Return the absolute path of a directory containing additional
         static resources (such as images, style sheets, etc)."""
@@ -87,17 +87,17 @@ class BittenNotifyDispatcher(Component):
 
 class BuildInfo(dict):
     """Wraps a Build instance and exposes properties conveniently"""
-    
+
     readable_states = {Build.SUCCESS:'Successful', Build.FAILURE:'Failed'}
-    
+
     def __init__(self, env, build):
         dict.__init__(self)
         self.build = build
         self.env = env
-        self['project_name'] = self.env.project_name 
+        self['project_name'] = self.env.project_name
         self['id'] = self.build.id
         self['status'] = self.readable_states[self.build.status]
-        self['link'] = self.env.abs_href.build(self.build.config, 
+        self['link'] = self.env.abs_href.build(self.build.config,
                 self.build.id)
         self['config'] = self.build.config
         self['slave'] = self.build.slave
@@ -106,45 +106,45 @@ class BuildInfo(dict):
         self['author'] = self.get_author(build)
         self['errors'] = self.get_errors(build)
         self['faillog'] = self.get_faillog(build)
-        
+
     def get_author(self, build):
         if build and build.rev:
             changeset = self.env.get_repository().get_changeset(build.rev)
             return changeset.author
-            
+
     def get_failed_steps(self, build):
-        build_steps = BuildStep.select(self.env, 
-                build=build.id, 
+        build_steps = BuildStep.select(self.env,
+                build=build.id,
                 status=BuildStep.FAILURE)
         return build_steps
-        
+
     def get_errors(self, build):
         errors = ''
         for step in self.get_failed_steps(build):
             errors += ', '.join(['%s: %s' % (step.name, error) \
                     for error in step.errors])
         return errors
-                               
+
     def get_faillog(self, build):
         faillog = ''
         for step in self.get_failed_steps(build):
-            build_logs = BuildLog.select(self.env, 
-                    build=build.id, 
+            build_logs = BuildLog.select(self.env,
+                    build=build.id,
                     step=step.name)
             for log in build_logs:
                 faillog += '\n'.join(['%5s: %s' % (level, msg) \
                         for level, msg in log.messages])
         return faillog
-                               
+
     def __getattr__(self, attr):
         return dict.__getitem__(self,attr)
-    
+
     def __repr__(self):
         repr = ''
         for k, v in self.items():
             repr += '%s: %s\n' % (k, v)
         return repr
-    
+
     def __str__(self):
         return self.repr()
 
@@ -154,7 +154,7 @@ class BittenNotifyEmail(NotifyEmail):
 
     template_name = 'bitten_notify_email.txt'
     from_email = 'bitten@localhost'
-    
+
     def __init__(self, env):
         NotifyEmail.__init__(self, env)
 
@@ -162,12 +162,12 @@ class BittenNotifyEmail(NotifyEmail):
         self.build_info = build_info
         self.data = self.build_info
         subject = '[%s Build] %s [%s] %s' % (self.build_info.status,
-                self.env.project_name, 
-                self.build_info.changeset, 
+                self.env.project_name,
+                self.build_info.changeset,
                 self.build_info.config)
-        stream = self.template.generate(**self.data) 
-        body = stream.render('text') 
-        self.env.log.debug('notification: %s' % body )         
+        stream = self.template.generate(**self.data)
+        body = stream.render('text')
+        self.env.log.debug('notification: %s' % body )
         NotifyEmail.notify(self, self.build_info.id, subject)
 
     def get_recipients(self, resid):
--- bitten/tests/notify.py
+++ bitten/tests/notify.py
@@ -4,7 +4,7 @@
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
-# you should have received as part of this distribution. 
+# you should have received as part of this distribution.
 
 import logging
 import os
@@ -21,14 +21,14 @@ ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 class BittenNotifyBaseTest(unittest.TestCase):
     def setUp(self):
         self.set_up_env()
-        
+
     def set_up_env(self):
         self.env = EnvironmentStub()
         self.env.get_templates_dir = lambda *args: os.path.join(ROOT, 'bitten', 'templates')
         self.env.path = ''
         self.repos = Mock(get_changeset=lambda rev: Mock(author = 'author'))
         self.env.get_repository = lambda authname = None: self.repos
-        
+
         db = self.env.get_db_cnx()
         cursor = db.cursor()
         connector, _ = DatabaseManager(self.env)._get_connector()
@@ -49,66 +49,66 @@ class BittenNotifyDispatcherTest(BittenNotifyBaseTest):
         self.dispatcher.email = self.email
         self.failed_build = Build(self.env, status = Build.FAILURE)
         self.successful_build = Build(self.env, status = Build.SUCCESS)
-        
+
     def test_do_notify_on_failed_build(self):
         self.env.config.set(CONFIG_SECTION, NOTIFY_ON_FAILURE, 'true')
         self.dispatcher.notify(self.failed_build)
-        self.assertTrue(self.state[0], 
+        self.assertTrue(self.state[0],
                 'notifier should be called for failed builds.')
-        
+
     def test_do_not_notify_on_failed_build(self):
         self.env.config.set(CONFIG_SECTION, NOTIFY_ON_FAILURE, 'false')
         self.dispatcher.notify(self.failed_build)
-        self.assertFalse(self.state[0], 
+        self.assertFalse(self.state[0],
                 'notifier should not be called for failed build.')
-        
+
     def test_do_notify_on_successful_build(self):
         self.env.config.set(CONFIG_SECTION, NOTIFY_ON_SUCCESS, 'true')
         self.dispatcher.notify(self.successful_build)
-        self.assertTrue(self.state[0], 
+        self.assertTrue(self.state[0],
                 'notifier should be called for successful builds when configured.')
 
     def test_do_not_notify_on_successful_build(self):
         self.env.config.set(CONFIG_SECTION, NOTIFY_ON_SUCCESS, 'false')
         self.dispatcher.notify(self.successful_build)
-        self.assertFalse(self.state[0], 
+        self.assertFalse(self.state[0],
                 'notifier shouldn\'t be called for successful build.')
-        
+
 
 class BuildInfoTest(BittenNotifyBaseTest):
     """unit tests for BuildInfo class"""
-    
+
     def setUp(self):
         BittenNotifyBaseTest.setUp(self)
         #fixture
-        self.failed_build = Build(self.env, 
-                config = 'config', 
+        self.failed_build = Build(self.env,
+                config = 'config',
                 slave = 'slave',
                 rev = 10,
                 status = Build.FAILURE)
         self.failed_build.id = 1
-        self.successful_build = Build(self.env, status = Build.SUCCESS) 
+        self.successful_build = Build(self.env, status = Build.SUCCESS)
         self.successful_build.id = 2
-        step = BuildStep(self.env, 
-                build = 1, 
-                name = 'test', 
+        step = BuildStep(self.env,
+                build = 1,
+                name = 'test',
                 status = BuildStep.FAILURE)
         step.errors = ['msg']
         step.insert()
         log = BuildLog(self.env, build = 1, step = 'test')
         log.messages = [('info','msg')]
         log.insert()
-        
+
     def test_exposed_properties(self):
         build_info = BuildInfo(self.env, self.failed_build)
         self.assertEquals(self.failed_build.id, build_info.id)
         self.assertEquals('Failed', build_info.status)
-        self.assertEquals('http://example.org/trac.cgi/build/config/1', 
+        self.assertEquals('http://example.org/trac.cgi/build/config/1',
                 build_info.link)
         self.assertEquals('config', build_info.config)
         self.assertEquals('slave', build_info.slave)
         self.assertEquals('10', build_info.changeset)
-        self.assertEquals('http://example.org/trac.cgi/changeset/10', 
+        self.assertEquals('http://example.org/trac.cgi/changeset/10',
                 build_info.changesetlink)
         self.assertEquals('author', build_info.author)
         self.assertEquals('test: msg', build_info.errors)
@@ -133,21 +133,21 @@ class BittenNotifyEmailTest(BittenNotifyBaseTest):
         self.email.finish_send = empty
         self.email.send = lambda to, cc, hdrs = {} : \
                 self.state.__setitem__(0,to)
-        self.build_info = BuildInfo(self.env, Build(self.env, 
+        self.build_info = BuildInfo(self.env, Build(self.env,
                 status = Build.SUCCESS))
         self.build_info['author'] = 'author'
-        
+
     def test_notification_uses_default_address(self):
         self.email.notify(self.build_info)
-        self.assertTrue('author' in self.state[0], 
+        self.assertTrue('author' in self.state[0],
                 'recipient list should contain plain author')
-        
+
     def test_notification_uses_custom_address(self):
         self.env.get_known_users = lambda cnx = None : [('author',
                 'Author\'s Name',
                 'author@email.com')]
         self.email.notify(self.build_info)
-        self.assertTrue('author@email.com' in self.state[0], 
+        self.assertTrue('author@email.com' in self.state[0],
                 'recipient list should contain custom author\'s email')
 
     def test_notification_discards_invalid_address(self):
@@ -155,9 +155,9 @@ class BittenNotifyEmailTest(BittenNotifyBaseTest):
                 'Author\'s Name',
                 '')]
         self.email.notify(self.build_info)
-        self.assertTrue('author' in self.state[0], 
+        self.assertTrue('author' in self.state[0],
                 'recipient list should only use valid custom address')
-        
+
 
 def suite():
     suite = unittest.TestSuite()
