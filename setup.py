5d7b8066865f16f6607ca0f4984903b2ff6063b3
osimons <osimons@2d6df8db-84f7-0310-ba0f-a4c632ebc3b5>
2009-09-10 12:15:40 +0000

    Resetting version and changelog information in trunk.
    git-svn-id: http://svn.edgewall.org/repos/bitten/trunk@756 2d6df8db-84f7-0310-ba0f-a4c632ebc3b5

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ recipe_commands = [
         NS + 'xml#transform = bitten.build.xmltools:transform'
     ]
 shared_args = {
-        'version': '0.6',
+        'version': '0.7',
         'author': 'Edgewall Software',
         'author_email': 'info@edgewall.org',
         'license': 'BSD',
