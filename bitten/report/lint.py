586f9bc2316305e2c8342e9760e61652ca226026
dfraser <dfraser@2d6df8db-84f7-0310-ba0f-a4c632ebc3b5>
2009-09-17 09:26:42 +0000

    Adding `report.id` to the `group by` to fix for postgresql - see #451
    git-svn-id: http://svn.edgewall.org/repos/bitten/trunk@769 2d6df8db-84f7-0310-ba0f-a4c632ebc3b5

--- bitten/report/lint.py
+++ bitten/report/lint.py
@@ -45,7 +45,7 @@ select build.rev,
 from bitten_report as report
  left outer join bitten_build as build ON (report.build=build.id)
 where build.config='%s' and report.category='lint'
-group by build.rev_time, build.rev, build.platform
+group by build.rev_time, build.rev, build.platform, report.id
 order by build.rev_time;""" % (config.name,)
 
         #self.log.debug('sql=\'%s\'' % (query,))
